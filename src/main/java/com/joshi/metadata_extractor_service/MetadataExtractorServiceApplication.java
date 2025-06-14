0819bdb284028a4f5fc42b1c29ac8ce7e356ea7b
JoshiNagendra <joshichennamsetti999@gmail.com>
2025-06-14 23:42:13 +0530

    Initial commit for metadata extractor service

---
+++ src/main/java/com/joshi/metadata_extractor_service/MetadataExtractorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.joshi.metadata_extractor_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetadataExtractorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetadataExtractorServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joshi/metadata_extractor_service/MetadataExtractorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joshi.metadata_extractor_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetadataExtractorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
